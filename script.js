37e51e85e4f29c5e5f41284b6c4ebed751b5f727
antonio mikael <antoniomikael500@gmail.com>
2024-06-09 21:46:51 -0300

    validação input contato

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById("phone").addEventListener("input", function (e) {
-  var x = this.value.replace(/\D/g, "").match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
-  this.value = !x[2]
-    ? x[1]
-    : "(" + x[1] + ")" + x[2] + (x[3] ? "-" + x[3] : "");
+    var x = this.value.replace(/\D/g, "").match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+    this.value = !x[2]
+        ? x[1]
+        : "(" + x[1] + ")" + x[2] + (x[3] ? "-" + x[3] : "");
 });
 
 const inputPhone = document.getElementById("phone");
@@ -13,26 +13,27 @@ const phone = [];
 let linhas = "";
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  if (nomes.includes(inputNome.value)) {
-    alert("Contato ja foi inserido");
-  }
-  else if (phone.includes(inputPhone.value)) {
-    alert("Contato ja foi inserido");
+    if (inputPhone.value.length !== 14) {
+        alert("Numero digitado é invalido");
+    } else {
+        if (nomes.includes(inputNome.value)) {
+            alert("Contato ja foi inserido");
+        }
+        if (phone.includes(inputPhone.value)) {
+            alert("Contato ja foi inserido");
+        } else {
+            let linha = "<tr>";
+            linha += `<td>${inputNome.value}</td>`;
+            linha += `<td>${inputPhone.value}</td>`;
+            linha += "</tr>";
+            linhas += linha;
 
-  }     
-   else {
-    let linha = "<tr>";
-    linha += `<td>${inputNome.value}</td>`;
-    linha += `<td>${inputPhone.value}</td>`;
-    linha += "</tr>";
-    linhas += linha;
-
-    document.querySelector("tbody").innerHTML = linhas;
-    nomes.push(inputNome.value);
-    phone.push(inputPhone.value);
-    document.getElementById('qtdeContato').innerHTML = nomes.length;
-  }
-
+            document.querySelector("tbody").innerHTML = linhas;
+            nomes.push(inputNome.value);
+            phone.push(inputPhone.value);
+            document.getElementById("qtdeContato").innerHTML = nomes.length;
+        }
+    }
 });
